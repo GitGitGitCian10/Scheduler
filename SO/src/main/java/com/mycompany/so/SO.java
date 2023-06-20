169f6c8d39fbdd4db2b1521cabf39c6d1712c59d
GitGitGitCian10 <thevisanti@gmail.com>
2023-06-20 19:30:26 -0300

    Codigo final

--- SO/src/main/java/com/mycompany/so/SO.java
+++ SO/src/main/java/com/mycompany/so/SO.java
@@ -4,6 +4,10 @@ public class SO {
 
     public static void main(String[] args) {
         Scheduler scheduler = new Scheduler();
-        scheduler.Simulacion();
+        scheduler.AddProcess("jamon", 2, 0);
+        scheduler.AddProcess("queso", 3, 2);
+        scheduler.AddProcess("pan", 5, 1);
+        scheduler.AddProcess("mayonesa", 2, 4);
+        scheduler.Simulacion(true);
     }
 }
--- SO/src/main/java/com/mycompany/so/Scheduler.java
+++ SO/src/main/java/com/mycompany/so/Scheduler.java
@@ -1,19 +1,22 @@
 package com.mycompany.so;
 import java.util.ArrayDeque;
-import java.util.LinkedList;
 import java.util.Iterator;
 import java.util.Scanner;
-import java.util.Timer;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Scheduler{
     ArrayDeque<Process> colaListo = new ArrayDeque<Process>();
     ArrayDeque<Process> colaEspera = new ArrayDeque<Process>();
-    Process enEjecucion;
+    Process enEjecucion = null;
     int procesosCompletados = 0;
+    int cantCiclos = 0;
     final int quantumBase = 4;
-    int quantumActual;
-    Timer timer = new Timer();
+    int quantumActual = quantumBase;
+    ScheduledExecutorService timer = Executors.newScheduledThreadPool(1);
+    boolean finalizado = false;
     
     public void AddProcess(String nombre, int tiempoEjecucion, int tiempoLlegada) {
         Process p = new Process(nombre, tiempoEjecucion, tiempoLlegada);
@@ -24,75 +27,70 @@ public class Scheduler{
         colaEspera.add(p);
     }
     
-    public void ChequearListos() {
-        Iterator<Process> iter = colaEspera.iterator();
-        if(iter.hasNext()) {
-            Process p = iter.next();
-            if(p.EstaListo()) {
-                iter.remove();
-                colaListo.add(p);
-            }
-        }
-        enEjecucion = colaListo.poll();
-    }
-    
     public boolean NoMasProcesos() {
         return colaListo.isEmpty() && colaEspera.isEmpty();
     }
     
     public void Simulacion() {
+        Simulacion(false);
+    }
+    
+    public void Simulacion(boolean automatico){
         Scanner scan = new Scanner(System.in);
-        System.out.print('>');
-        String op = scan.next();
-        String pop = null;       
-        while(!op.equalsIgnoreCase("a") && !op.equalsIgnoreCase("e")) {
-            System.out.print('>');
-            op = scan.next();
-            while(op.equalsIgnoreCase("p") && !pop.equalsIgnoreCase("pe")) {
+        while(!finalizado) {
+            String op;
+            if(automatico) {
+                op = "a";
+            } else {
                 System.out.print('>');
-                pop = scan.next();
-                String[] p = pop.split(",");
-                AddProcess(p[0], Integer.parseInt(p[1]), Integer.parseInt(p[2]));
+                op = scan.next();
             }
-            CicloScheduler();
+            switch(op) {
+                case "p":
+                    System.out.print('>');
+                    String pop = scan.next();
+                    while(!pop.equalsIgnoreCase("pe")) {
+                        String[] p = pop.split(",");
+                        AddProcess(p[0], Integer.parseInt(p[1]), Integer.parseInt(p[2]));
+                        System.out.println("Añadido el proceso '" + p[0] + "'");
+                        System.out.print('>');
+                        pop = scan.next();
+                    }
+                    break;
+                case "e":
+                    finalizado = true;
+                    break;
+                case "a":
+                    timer.scheduleAtFixedRate(new TimerTask() {
+                        @Override
+                        public void run() {
+                            CicloScheduler();
+                        }
+                    }, 1, 1, TimeUnit.SECONDS);
+                    try {
+                    timer.awaitTermination(1, TimeUnit.DAYS);
+                    } catch(InterruptedException ex) {
+                    }
+                    break;
+
+                default:
+                    CicloScheduler();
+                    break;
+            } 
         }
-        if(op.equalsIgnoreCase("e")) return;
-        if(colaEspera.isEmpty()) {
-            System.out.println("¡El scheduler no tiene tareas!");
-            return;
-        }
-        ChequearListos();
-        quantumActual = quantumBase;
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                CicloScheduler();
-            }
-        }, 1000, 1000);
-        op = "e";
+        System.out.println("Procesos completados: " + procesosCompletados);
+        System.out.println("Ciclos empleados: " + cantCiclos);
+        
     }
     
     public void CicloScheduler() {
         if(NoMasProcesos() && (enEjecucion == null || enEjecucion.EstaFinalizado())) {
-            timer.cancel();
+            timer.shutdownNow();
+            finalizado = true;
             return; 
         }
         
-        if(enEjecucion == null) {
-            enEjecucion = colaListo.poll();
-        } else if(enEjecucion.EstaFinalizado()) {
-            procesosCompletados++;
-            if(!NoMasProcesos()) {     
-                enEjecucion = colaListo.poll();
-                quantumActual = quantumBase;
-            }
-        }
-        
-        if(quantumActual == 0) {
-            colaListo.add(enEjecucion);
-            enEjecucion = colaListo.poll();
-            quantumActual = quantumBase;
-        }
+        System.out.println("-".repeat(14) + "Ciclo " + (cantCiclos + 1) + "-".repeat(14));
         
         Iterator<Process> iter = colaEspera.iterator();
         while(iter.hasNext()) {
@@ -104,9 +102,34 @@ public class Scheduler{
             }
         }
         
+        if(enEjecucion == null) {
+            enEjecucion = colaListo.poll();
+            if(enEjecucion != null) System.out.println("Comenzando el proceso '" + enEjecucion.getNombre() + "' (" + enEjecucion.getTiempoEjecucion() + ")");;
+        } else if(enEjecucion.EstaFinalizado()) {
+            //
+            if(!NoMasProcesos()) {     
+                enEjecucion = colaListo.poll();
+                if(enEjecucion != null) System.out.println("Comenzando el proceso '" + enEjecucion.getNombre() + "' (" + enEjecucion.getTiempoEjecucion() + ")");;
+                quantumActual = quantumBase;
+            }
+        }
+        
+        if(quantumActual == 0) {
+            colaListo.add(enEjecucion);
+            System.out.println("Timeout al proceso '" + enEjecucion.getNombre() + "' (" + enEjecucion.getTiempoEjecucion() + ")");
+            enEjecucion = colaListo.poll();
+            if(enEjecucion != null) System.out.println("Comenzando el proceso '" + enEjecucion.getNombre() + "' (" + enEjecucion.getTiempoEjecucion() + ")");;
+            quantumActual = quantumBase;
+        }
+        
         if(enEjecucion != null) {
             enEjecucion.CicloEnEjecucion();
+            if(enEjecucion.EstaFinalizado()) {
+                procesosCompletados++;
+                System.out.println("Finalizado el proceso '" + enEjecucion.getNombre() + "' (" + enEjecucion.getTiempoEjecucion() + ")");
+            }
             quantumActual--;
         }
+        cantCiclos++;
     }
 }
